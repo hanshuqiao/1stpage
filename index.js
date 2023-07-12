f2fed37c20e9800b2b9a6ffbdea81e05c6301f2e
hanshuqiao <138348187+hanshuqiao@users.noreply.github.com>
2023-07-12 14:35:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ S.UI = (function () {
           break;
 
         case 'icon':
-          S.ShapeBuilder.imageFile('1stpage/'+value + '.png', function (obj) {
+          S.ShapeBuilder.imageFile(value + '.png', function (obj) {
             S.Shape.switchShape(obj);
           });
           break;
